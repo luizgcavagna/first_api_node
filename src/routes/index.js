c10c861bbbc8075c3876dbcfad282646521a9d6d
Luiz Gustavo Cavagna <luizcavgana@gmail.com>
2024-12-04 20:22:05 -0300

    routes and controller

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const { Router } = require('express');
+
+const usersRoutes = require('./user.routes');
+
+const routes = Router();
+
+/*
+routes.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
+routes.get('/route-params/:id/:user', (request, response) => {
+    const { id, user } = request.params;
+
+    response.send(`O id: ${id} é do usuário: ${user}`);
+});
+
+routes.get('/query-params', (request, response) => {
+    const { page, limit } = request.query;
+
+    response.send(`A página é: ${page} e o limite: ${limit}`);
+});
+*/
+
+routes.use('/users', usersRoutes);
+
+module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,29 +1,11 @@
 const express = require("express");
 
+const routes = require('./routes')
+
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response) => {
-    response.send('Hello World!');
-});
-
-app.get('/route-params/:id/:user', (request, response) => {
-    const { id, user } = request.params;
-
-    response.send(`O id: ${id} é do usuário: ${user}`);
-});
-
-app.get('/query-params', (request, response) => {
-    const { page, limit } = request.query;
-
-    response.send(`A página é: ${page} e o limite: ${limit}`);
-});
-
-app.post('/post', (request, response) => {
-    const { name, email, password } = request.body;
-
-    response.json({ name, email, password })
-});
+app.use(routes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
