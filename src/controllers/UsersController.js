a67a25e720208d728c2fb78502370fdf324a279e
Luiz Gustavo Cavagna <luizcavgana@gmail.com>
2024-12-04 21:26:04 -0300

    controllers

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,6 +6,11 @@ class UsersController {
    * update - PUT para atualizar um registro.
    * delete - DELETE para remover um registro.
    */
+   create(request, response) {
+      const { name, email, password } = request.body;
+
+      response.json({ name, email, password })
+   }
 }
 
 module.exports = UsersController;
