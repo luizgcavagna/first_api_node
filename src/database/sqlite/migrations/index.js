0f895c0989cab3b1842d42da267a903a42b4be77
Luiz Gustavo Cavagna <luizcavgana@gmail.com>
2024-12-06 18:34:45 -0300

    migration

---
+++ src/database/sqlite/migrations/index.js
@@ -0,0 +1,15 @@
+const sqliteConnection = require('../../sqlite');
+const creatUsers = require('./createUsers');
+
+function migrationsRun() {
+  const schemas = [
+    creatUsers
+  ].join('');
+
+  sqliteConnection()
+  .then(db => db.exec(schemas))
+  .catch(error => console.error(error));
+
+}
+
+module.exports = migrationsRun;
